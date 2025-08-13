f9764cc4cdbce1373d6c2d1523b2dd8f46747ab2
Isaque de Sousa <isaque.error404@gmail.com>
2025-08-13 17:49:23 -0300

    feat: config drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("The DATABASE_URL env is required");
+  throw new Error("The DATABASE_URL env is required.");
 }
 
 export default defineConfig({
@@ -9,4 +9,6 @@ export default defineConfig({
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
+  out: "./drizzle",
+  schema: "./src/database/schema.ts",
 });
