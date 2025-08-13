0b8e19eebd832c8983eb085e39fcd175da80188a
Isaque de Sousa <isaque.error404@gmail.com>
2025-08-13 19:35:41 -0300

    feat: add query in db

--- server.ts
+++ server.ts
@@ -1,5 +1,7 @@
+import { eq } from "drizzle-orm";
 import fastify from "fastify";
-import crypto from "node:crypto";
+import { db } from "./src/database/client.ts";
+import { courses } from "./src/database/schema.ts";
 
 const server = fastify({
   logger: {
@@ -13,17 +15,13 @@ const server = fastify({
   },
 });
 
-const courses = [
-  { id: "1", title: "Curso de Node.js" },
-  { id: "2", title: "Curso de React" },
-  { id: "3", title: "Curso de React Native" },
-];
+server.get("/courses", async (request, reply) => {
+  const result = await db.select().from(courses);
 
-server.get("/courses", (request, reply) => {
-  return reply.send({ courses });
+  return reply.send({ courses: result });
 });
 
-server.get("/courses/:id", (request, reply) => {
+server.get("/courses/:id", async (request, reply) => {
   type Params = {
     id: string;
   };
@@ -31,16 +29,19 @@ server.get("/courses/:id", (request, reply) => {
   const params = request.params as Params;
   const courseId = params.id;
 
-  const course = courses.find((course) => course.id === courseId);
+  const result = await db
+    .select()
+    .from(courses)
+    .where(eq(courses.id, courseId));
 
-  if (course) {
-    return { course };
+  if (result.length > 0) {
+    return { course: result[0] };
   }
 
   return reply.status(404).send();
 });
 
-server.post("/courses", (request, reply) => {
+server.post("/courses", async (request, reply) => {
   type Body = {
     title: string;
   };
@@ -54,9 +55,14 @@ server.post("/courses", (request, reply) => {
     return reply.status(400).send({ message: "Título obrigatório." });
   }
 
-  courses.push({ id: courseId, title: courseTitle });
+  const result = await db
+    .insert(courses)
+    .values({
+      title: courseTitle,
+    })
+    .returning();
 
-  return reply.status(201).send({ courseId });
+  return reply.status(201).send({ courseId: result[0].id });
 });
 
 server.listen({ port: 3333 }).then(() => {
