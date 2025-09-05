9ea57303ca45011f012fc44469f51a63f52b9992
Isaque de Sousa <isaque.error404@gmail.com>
2025-09-05 13:19:05 -0300

    test: add unit test for getting course by id

---
+++ src/routes/get-course-by-id.test.ts
@@ -0,0 +1,21 @@
+import request from "supertest";
+import { expect, test } from "vitest";
+import { server } from "../app.ts";
+import { makeCourse } from "../tests/factories/make-course.ts";
+
+test("get course by id", async () => {
+  await server.ready();
+
+  const course = await makeCourse();
+
+  const response = await request(server.server).get(`/courses/${course.id}`);
+
+  expect(response.status).toEqual(200);
+  expect(response.body).toEqual({
+    course: {
+      id: expect.any(String),
+      title: expect.any(String),
+      description: null,
+    },
+  });
+});
