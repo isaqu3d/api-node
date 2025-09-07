db26be7112b9a6c7ae873b3720eac4c016904870
Isaque de Sousa <isaque.error404@gmail.com>
2025-09-07 09:47:36 -0300

    feat: add JWT request validation middleware

---
+++ src/routes/hooks/check-request-jwt.ts
@@ -0,0 +1,25 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import jwt from "jsonwebtoken";
+
+type JWTPayload = {
+  sub: string;
+  role: "student" | "manager";
+};
+
+export function checkRequestJwt(request: FastifyRequest, reply: FastifyReply) {
+  const token = request.headers.authorization;
+
+  if (!token) {
+    return reply.status(401).send();
+  }
+
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET must be set.");
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return reply.status(401).send();
+  }
+}
