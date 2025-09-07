3256bc4f3e6c0b83112b040e0e731e4517f6992c
Isaque de Sousa <isaque.error404@gmail.com>
2025-09-07 10:00:03 -0300

    feat: implement preHandler for CreateCourseRoute with JWT and role-based access control

--- src/routes/hooks/check-user-role.ts
+++ src/routes/hooks/check-user-role.ts
@@ -1,11 +1,11 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { getAuthenticatedUserFromRequest } from "../../utils/get-authenticated-user-from-request.ts";
 
-export function checkUserRole() {
+export function checkUserRole(role: "student" | "manager") {
   return async function (request: FastifyRequest, reply: FastifyReply) {
     const user = getAuthenticatedUserFromRequest(request);
 
-    if (user.role !== "manager") {
+    if (user.role !== role) {
       return reply.status(401).send();
     }
   };
