233c0104bdbac8b497c1c90690900a42de5b6837
Isaque de Sousa <isaque.error404@gmail.com>
2025-09-09 10:47:53 -0300

    fix: removed drizzle

--- drizzle/0006_groovy_the_leader.sql
+++
@@ -1,3 +0,0 @@
-CREATE TYPE "public"."user_role" AS ENUM('student', 'manager');--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "password" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "role" "user_role" DEFAULT 'student' NOT NULL;
